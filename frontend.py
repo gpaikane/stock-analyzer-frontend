ebd42f12181448bc0beb0bdbaae3dc770dee09bb
gpaikane <guruppk46@gmail.com>
2025-04-18 18:58:07 +0530

    modified frontend.py

--- frontend.py
+++ frontend.py
@@ -189,7 +189,6 @@ if __name__ == "__main__":
         fig = plots.plot_ploty(pd.DataFrame(forecast), ticker=ticker)
         st.plotly_chart(fig, use_container_width=True)
 
-        logging.info(len(fundamentals_values) != 0)
         if news_summary is not None or (fundamentals_values is not None and len(fundamentals_values) != 0):
             if news_summary is None:
                 news_summary = ""
